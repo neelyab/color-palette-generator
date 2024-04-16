789ffdb78641fd0af4a9d3b968573f81d6c50b15
Amanda Neely <46635521+neelyab@users.noreply.github.com>
2024-04-16 14:16:42 -0400

    updated readme and test fixture

--- src/db/fixtures/colors.js
+++ src/db/fixtures/colors.js
@@ -2,12 +2,12 @@ const colorList = require('../../../test/dmcColors');
 
 const finalList = colorList.map((color)=> {
     let colorInfo = {};
-    colorInfo.color_name = color.dmcName;
-    colorInfo.color_code = color.dmcColor;
+    colorInfo.color_name = color.color_name;
+    colorInfo.color_code = color.color_code;
     colorInfo.r = color.r;
     colorInfo.g = color.g;
     colorInfo.b = color.b;
-    colorInfo.hex_code = color.hexCode;
+    colorInfo.hex_code = color.hex_code;
     return colorInfo;
 });
 
